8b385f782bb3cfb1dac458422937106a95b27ddd
Gabriel Sá <gabrieljensensa@gmail.com>
2020-10-06 23:44:21 -0300

    added zezao in response

--- core/views.py
+++ core/views.py
@@ -12,6 +12,7 @@ class TestIsUp(APIView):
       "status": "OK",
       "Micaelli": "A Micaelli é uma gostosa e nós amamos ela. Ass: Sá e Luis :) <3",
       "Luis": "O Luis tbm!",
+      "Zezao": "O Zezão está fazendo um freela para um projeto da Tiemi que não sei o que é, mas logo descobrirei",
     }
 
     return Response(response, status=200)
